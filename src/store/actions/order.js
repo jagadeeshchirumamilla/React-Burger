a04bbdc3e310f289b9b118bb310defac30918fd6
JagadeeshChirumamilla <jchirumamilla@xebia.com>
2018-06-21 14:22:22 +0530

    guarding routes

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -26,6 +26,7 @@ export const purchaseBurger = (orderData,token) =>{
     return dispatch =>{
 
         dispatch(purcharseBurgerStart());
+        
         axios.post( '/orders.json?auth='+token, orderData )
             .then( response => {
                 console.log(response.data);
@@ -64,10 +65,11 @@ export const fetchOrdersStart = () =>{
     };
 };
 
-export const fetchOrders = (token) =>{
+export const fetchOrders = (token,userId) =>{
     return dispatch => {
         dispatch(fetchOrdersStart());
-        axios.get('/orders.json?auth='+token)
+        const queryParams = '?auth='+token+'&orderBy="userId"&equalTo="'+userId+'"';
+        axios.get('/orders.json'+queryParams)
         .then(res =>{
             const fetchedOrders = [];
             for(let key in res.data){
